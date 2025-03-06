bc1622b91368f1794d3858a5514d9bb9fcff5e09
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-03-07 01:26:48 +0700

    Add hooks for fetching analytics data from various endpoints

---
+++ hooks/useGetClicksContent.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { clientApiRequest } from "@/services/clientApiRequest";
+import { ApiResponse } from "@/app/Get/dataTypes";
+
+const fetchUrls = async (): Promise<ApiResponse> => {
+  try {
+    return await clientApiRequest<ApiResponse>({
+      method: "GET",
+      endpoint: "analytics/content",
+    });
+  } catch (error) {
+    console.error("Error fetching URLs:", error);
+    throw new Error("Failed to fetch URLs");
+  }
+};
+
+// ✅ Hook React Query untuk mengambil URL + Link Preview
+export const useGetUrls = () => {
+  return useQuery<ApiResponse>({
+    queryKey: ["analytics/content"], // Key unik untuk caching
+    queryFn: fetchUrls,
+    staleTime: 1000 * 60 * 5, // Data akan dianggap fresh selama 5 menit
+    refetchOnWindowFocus: false, // Tidak auto-refresh saat pindah tab
+  });
+};
