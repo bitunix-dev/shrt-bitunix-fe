13ce9425ccceca8d7f6867838cbe5a133bbc2919
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-03-14 17:42:20 +0700

    refactor logout button for improved styling and functionality

--- components/BtnLogout.tsx
+++ components/BtnLogout.tsx
@@ -1,49 +1,56 @@
-'use client'
+"use client";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
-import { useRouter } from "next/navigation"
-import { logout } from "@/services/authServices"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { logout } from "@/services/authServices";
 
 export function BtnLogout() {
-    const router = useRouter()
+  const router = useRouter();
 
-    const handleLogout = async () => {
-        try {
-            await logout()
-            router.push('/login')
-        } catch (error) {
-            console.log(error)
-        }
+  const handleLogout = async () => {
+    try {
+      await logout();
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>
-                <Button variant="destructive">Log Out</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Logout Confirmation</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        {`Are you sure you want to log out of the application? 
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="bg-red-500 hover:bg-red-600 text-neutral-900 font-bold">
+          Log Out
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Logout Confirmation</AlertDialogTitle>
+          <AlertDialogDescription>
+            {`Are you sure you want to log out of the application?
                         You'll need to log in again to access the dashboard.`}
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction className="bg-[var(--bitunix)] text-black" onClick={handleLogout}>Logout</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-[var(--bitunix)] hover:bg-[var(--bitunix-hover)] text-black font-bold"
+            onClick={handleLogout}
+          >
+            Logout
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
