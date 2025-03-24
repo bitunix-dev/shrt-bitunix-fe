58b96cf0cb26e3389b4bfa4f794d7bcd100b00f1
bahari <haribahari71@gmail.com>
2025-03-24 20:56:53 +0700

    fix stle btn at dwonload qr code

--- components/qrCode/QrCode.tsx
+++ components/qrCode/QrCode.tsx
@@ -97,7 +97,7 @@ const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({ value, logo }) => {
       {/* Add download button */}
       <button
         onClick={handleDownload}
-        className="mt-4 px-6 py-3 bg-[var(--bitunix)] hover:bg-[var(--bitunix-hover)] text-black rounded-md flex items-center justify-center space-x-2"
+        className="mt-4 px-6 py-3 bg-[var(--bitunix)] hover:bg-[var(--bitunix-hover)] text-black rounded-md flex items-center justify-center space-x-2 mx-auto"
       >
         <svg
           className="w-5 h-5 text-black"
